a4c7db786bb57f054b9814ee5944e6bbdc4b7788
ahadsts9901 <ahadsts990@gmail.com>
2024-03-31 02:16:30 -0700

    secret

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,20 +4,21 @@ import GoogleProvider from 'next-auth/providers/google'
 import GithubProvider from 'next-auth/providers/github'
 
 const handler = NextAuth({
-    providers: [
-        FacebookProvider({
-          clientId: process.env.FACEBOOK_ID!,
-          clientSecret: process.env.FACEBOOK_SECRET!,
-        }),
-        GoogleProvider({
-          clientId: process.env.GOOGLE_ID!,
-          clientSecret: process.env.GOOGLE_SECRET!,
-        }),
-        GithubProvider({
-            clientId: process.env.GITHUB_ID!,
-            clientSecret: process.env.GITHUB_SECRET!,
-        })
-    ]
+  providers: [
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID!,
+      clientSecret: process.env.FACEBOOK_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET!
 })
 
 export { handler as GET, handler as POST }
